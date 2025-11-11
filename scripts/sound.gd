30d2c4dd104f0c7541b87e4c282d8bfa80197053
Marcin Sobczyk <msobczyk@syncad.com>
2025-11-11 21:56:17 +0100

    Move resources to separate dictionary ./resources

--- scripts/sound.gd
+++ scripts/sound.gd
@@ -4,7 +4,6 @@ var background_theme: AudioStreamPlayer =  AudioStreamPlayer.new()
 var audio_stream_resource: AudioStream = preload("res://assets/dark_mansion_theme.mp3")
 
 const _VOLUME_CHANGE_FACTOR_DB: float = 2.0
-const _VOLUME_VALUE_SENSITIVITY: float = 5.0
 
 
 func decrease_background_theme_volume(times: int = 1) -> void:
@@ -17,8 +16,8 @@ func increase_background_theme_volume(times: int = 1) -> void:
 		background_theme.volume_db += _VOLUME_CHANGE_FACTOR_DB
 
 
-func set_background_theme_volume(value: float = 0.0) -> void:
-	background_theme.volume_db = value/_VOLUME_VALUE_SENSITIVITY
+func set_background_theme_volume(value: float = 0.5) -> void:
+	background_theme.volume_db = linear_to_db(value)
 
 
 func disable_background_theme_volume() -> void:
@@ -33,4 +32,5 @@ func _ready() -> void:
 	background_theme.stream = audio_stream_resource
 	background_theme.stream.loop = true
 	background_theme.autoplay = true
+	background_theme.bus = "BackgroundMusic"
 	add_child(background_theme)
