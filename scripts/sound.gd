dd00a707802bb79ce2f18c0830fa98e84d7e3337
Marcin Sobczyk <msobczyk@syncad.com>
2025-11-11 20:36:24 +0100

    Background music volume regulation by slider

--- scripts/sound.gd
+++ scripts/sound.gd
@@ -4,6 +4,7 @@ var background_theme: AudioStreamPlayer =  AudioStreamPlayer.new()
 var audio_stream_resource: AudioStream = preload("res://assets/dark_mansion_theme.mp3")
 
 const _VOLUME_CHANGE_FACTOR_DB: float = 2.0
+const _VOLUME_VALUE_SENSITIVITY: float = 5.0
 
 
 func decrease_background_theme_volume(times: int = 1) -> void:
@@ -16,9 +17,20 @@ func increase_background_theme_volume(times: int = 1) -> void:
 		background_theme.volume_db += _VOLUME_CHANGE_FACTOR_DB
 
 
+func set_background_theme_volume(value: float = 0.0) -> void:
+	background_theme.volume_db = value/_VOLUME_VALUE_SENSITIVITY
+
+
+func disable_background_theme_volume() -> void:
+	background_theme.stream_paused = true
+
+
+func enable_background_theme_volume() -> void:
+	background_theme.stream_paused = false
+
+
 func _ready() -> void:
 	background_theme.stream = audio_stream_resource
 	background_theme.stream.loop = true
 	background_theme.autoplay = true
 	add_child(background_theme)
-	decrease_background_theme_volume(10)
